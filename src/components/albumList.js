35a19dd35a2108495aab188b820506e7f9cadeec
Sylvain Gogel <sylvain.gogel@gmail.com>
2019-01-15 14:30:21 +0100

    fix: add material dependencies

--- src/components/albumList.js
+++ src/components/albumList.js
@@ -1,25 +1,57 @@
 import React, {useEffect, useState} from 'react';
 
 const AlbumList = () => {
+  // this functionnal component got states using state hooks 
   const [albums, setAlbums] = useState([])
+  const [currentAlbum, setCurrentAlbum] = useState(null)
 
+  // useEffect hook allow to handle side effects inside render()
+  //  second param [] tells react to run it only once
+  //  in fact it bind the run of the effect on the change
+  //  of the named variables in the array
+  //  so when you do not specify a variable it will only be run once
+  //
+  //  If you do not specify the second argument, the effect will
+  //  be run on each render of the component (pretty bad in our case
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/albums')
     .then(response => response.json())
     .then(setAlbums);
   }, [])
 
-  const listItems = albums.map((a) =>
-    <li data-id={a.id}>{a.title}</li>
-  );
+  const loadAlbum = (id)  => {
+    fetch(`https://jsonplaceholder.typicode.com/albums/${id}`)
+    .then(response => response.json())
+    .then(setCurrentAlbum);
+  }
 
-  return (
-    <div className="AlbumList">
-      <ul>
-        {listItems}
-      </ul>
-    </div>
-  )
+  if(currentAlbum != null) {
+    return (
+      <>
+        <li key={currentAlbum.id}>
+          <h2>{currentAlbum.title}</h2>
+        </li>
+        <li>
+          <button onClick={e => {setCurrentAlbum(null)}}>Back</button>
+        </li>
+      </>
+      )
+  } else {
+
+    const listItems = albums.map((a) =>
+      <li key={a.id}><button onClick={e => {
+        loadAlbum(a.id)}
+      } >{a.title}</button></li>
+    );
+
+    return (
+      <div className="AlbumList">
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default AlbumList;
